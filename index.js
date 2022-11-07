fad3c66846cf1c57e8eee1dd2968aa3b383d5408
Dima Tar <tarasenkodima338@gmail.com>
2022-11-07 09:49:14 +0200

    Show diferends

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-let userName = 'Tom';
-let vikToma = 23;
-let vinPovnolitni = true;
-let movaNaYakiVinProgramye = 'C++';
-let tomMaeDomashnyTvaruny = true;
-let vudTwarunuToma = 'Lasui kit';
-let imyKotaToma = "Rym'yanui"
 
-console.log(userName)
-console.log(vikToma)
-console.log(vinPovnolitni)
-console.log(movaNaYakiVinProgramye)
-console.log(tomMaeDomashnyTvaruny)
-console.log(vudTwarunuToma)
-console.log(imyKotaToma)
+let userFirstName;
+
+const userLastName = 'Pitt';
+
+userFirstName = 'Tom';
+
+userLastName = 'Cruse';
+
+console.log(userFirstName, userLastName);
