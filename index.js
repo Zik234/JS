ed1c42b35301c79a9e04b20c288637954d88e03a
Dima Tar <tarasenkodima338@gmail.com>
2022-11-09 09:06:03 +0200

    08.11.22. class

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-let number=456;
-console.log(number);
+// console.log(true, ' && ', true, ' = ', true && true)
+// console.log(true, ' && ', false, ' = ', true && false)
+// console.log(false, ' && ', true, ' = ', false && true)
+// console.log(false, ' && ', false, ' = ', false && false)
 
-number += 6;
-console.log(number);
+// console.log(true, ' || ', true, ' = ', true || true)
+// console.log(true, ' || ', false, ' = ', true || false)
+// console.log(false, ' || ', true, ' = ', false || true)
+// console.log(false, ' || ', false, ' = ', false || false)
 
-number ++;
-console.log(number);
+// let string = "";
+// let number = 12;
+// let userName = "Brad";
+// let bool = null;
+// let resultRemainder = number & 5; //2
+// let result = string || number;
 
-number /=2;
-console.log(number);
+// //console.log(result);
 
-number -= 3;
-console.log(number);
+// console.log(!string);
 
-number --;
-console.log(number);
+// console.log()
+// let number = 46
 
-number %= 4;
-console.log(number);
+// if (number % 2 === 0) {
+//   console.log('even')
+// } else {
+//   console.log('odd')
+// }
 
-number += 'qwerty';
-console.log(number);
+const userAge = 18
+if (userAge >= 6 && userAge < 18) {
+  console.log('puple')
+} else {
+  console.log('not puple')
+}
