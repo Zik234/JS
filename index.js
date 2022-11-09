d91bfed9d0a2a96e788d766a42ab17a4ada796bd
Dima Tar <tarasenkodima338@gmail.com>
2022-11-09 09:25:26 +0200

    Init home work if else

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-// console.log(true, ' && ', true, ' = ', true && true)
-// console.log(true, ' && ', false, ' = ', true && false)
-// console.log(false, ' && ', true, ' = ', false && true)
-// console.log(false, ' && ', false, ' = ', false && false)
+// 1) Якщо значення змінної number позитивне або 0 - виводимо ''positive",
+// інакше - "negative"
 
-// console.log(true, ' || ', true, ' = ', true || true)
-// console.log(true, ' || ', false, ' = ', true || false)
-// console.log(false, ' || ', true, ' = ', false || true)
-// console.log(false, ' || ', false, ' = ', false || false)
-
-// let string = "";
-// let number = 12;
-// let userName = "Brad";
-// let bool = null;
-// let resultRemainder = number & 5; //2
-// let result = string || number;
-
-// //console.log(result);
-
-// console.log(!string);
-
-// console.log()
-// let number = 46
-
-// if (number % 2 === 0) {
-//   console.log('even')
-// } else {
-//   console.log('odd')
-// }
-
-const userAge = 18
-if (userAge >= 6 && userAge < 18) {
-  console.log('puple')
+const number1 = 18;
+const MINPOSITIV = 0;
+if (number1>=MINPOSITIV) {
+  console.log('positiv')
 } else {
-  console.log('not puple')
+  console.log('negativ')
+}
+
+//2) Є дві змінні - num1 і num2. Якщо num1>num2 - вивести результат віднімання,
+//інакше - вивести результат множення
+
+const num1 = 20;
+const num2 = 15;
+if(num1>num2){
+  console.log(num1 - num2);
+}else{
+  console.log(num1 * num2);
+}
+
+//3) Якщо значення змінної кратно(тобто ділеться без остачі) на 7 і одночасно кратна 3 - виводимо "you win!",
+// інакше - "you loose"
+
+const variables = 21;
+const permanent1 = 7;
+const permanent2 =3;
+if (variables%permanent1===0 && variables%permanent2===0) {
+  console.log('you win!');
+} else {
+  console.log('you loose!')
 }
