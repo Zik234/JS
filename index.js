9198bb78587783614bd97ff3e094dd7163399de3
Dima Tar <tarasenkodima338@gmail.com>
2022-11-10 09:04:14 +0200

    initial commit fun

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-// 1) Якщо значення змінної number позитивне або 0 - виводимо ''positive",
-// інакше - "negative"
+// let userInput1 = prompt('Enter first number: ', 2)
+// let userInput2 = prompt('Enter second number: ', 6)
 
-const number1 = 18;
-const MINPOSITIV = 0;
-if (number1>=MINPOSITIV) {
-  console.log('positiv')
-} else {
-  console.log('negativ')
-}
+// let userNumber1 = Number(userInput1);
+// let userNumber2 = Number(userInput2);
 
-//2) Є дві змінні - num1 і num2. Якщо num1>num2 - вивести результат віднімання,
-//інакше - вивести результат множення
+// if (userInput1==='' ||
+//     userInput2===''||
+//     userInput1===null || 
+//     userInput2===null ||
+//     isNaN(userInput1) ||
+//     isNaN(userInput2)
+//     ) {
+//   console.log('error');
+// } else {
+//   let summa = userNumber1+userNumber2;
+//   console.log(summa);
+// }
 
-const num1 = 20;
-const num2 = 15;
-if(num1>num2){
-  console.log(num1 - num2);
-}else{
-  console.log(num1 * num2);
-}
+// myFirstFunction() 
+// function myFirstFunction() {
+//   console.log('myFirstFunction')
+// }
+// myFirstFunction() 
 
-//3) Якщо значення змінної кратно(тобто ділеться без остачі) на 7 і одночасно кратна 3 - виводимо "you win!",
-// інакше - "you loose"
-
-const variables = 21;
-const permanent1 = 7;
-const permanent2 =3;
-if (variables%permanent1===0 && variables%permanent2===0) {
-  console.log('you win!');
-} else {
-  console.log('you loose!')
-}
+// const mySecondFunction = function(){
+//   console.log("!mySecondF")
+// }
