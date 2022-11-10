faf1c07b3dffa4e5564c0e12bd35c18b2d9782b5
Dima Tar <tarasenkodima338@gmail.com>
2022-11-10 21:14:57 +0200

    Input Home Work 10_11_22

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
-// let userInput1 = prompt('Enter first number: ', 2)
-// let userInput2 = prompt('Enter second number: ', 6)
+//1) написати функцію, яка повертає слово 'odd' або 'even', в залежності від прийнятого аргументу(число).
+//Значення для перевірки функції вводить користувач.
+// let userInput = prompt('enter a number');
 
-// let userNumber1 = Number(userInput1);
-// let userNumber2 = Number(userInput2);
-
-// if (userInput1==='' ||
-//     userInput2===''||
-//     userInput1===null || 
-//     userInput2===null ||
-//     isNaN(userInput1) ||
-//     isNaN(userInput2)
-//     ) {
+// if(checkUserInputNumber(userInput)){
+// const remaind= remainderFromDivision(Number(userInput));
+// const result=checkRemaind(remaind);
+// console.log(result);
+// }else{
 //   console.log('error');
-// } else {
-//   let summa = userNumber1+userNumber2;
-//   console.log(summa);
+// }
+// function checkRemaind(result){
+//   if(result === 0){
+// return 'even'
+//   }else{
+//     return 'odd'
+//   }
+
 // }
 
-// myFirstFunction() 
-// function myFirstFunction() {
-//   console.log('myFirstFunction')
+// function remainderFromDivision(val){
+//   return val%=2;
 // }
-// myFirstFunction() 
 
-// const mySecondFunction = function(){
-//   console.log("!mySecondF")
+// function checkUserInputNumber(value){
+// if(value === ' ' || isNaN(value) || value === null ){
+// return false;
 // }
+// return true;
+// }
+
+//2)написати функцію, яка приймає два аргументи і, якщо аргументи одного типу, то повертає той,
+// який має більше значення (якщо значення рівні - повертаємо null), інакше(якщо різного типу) теж повертає null.
+
+const userInput1 = 4;
+const userInput2 = 6;
+if(comparison (userInput1, userInput2)){
+const more= comparisonResult(userInput1,userInput2);
+console.log(more);
+}else{
+  console.log('noll');
+}
+function comparisonResult(result1,result2){
+  if(result1 > result2){
+return result1
+  }else{
+    return result2
+  }
+}
+
+function comparison(val1,val2){if (typeof(val1) === typeof(val2)){
+  return true;
+}else{
+  return false;
+  }
+}
