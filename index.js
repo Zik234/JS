b7a510f82452b8ba3f72259daa7e4cc5ee159603
Dima Tar <tarasenkodima338@gmail.com>
2022-11-16 15:40:15 +0200

    Input Home Work for while

--- index.js
+++ index.js
@@ -9,39 +9,38 @@
 // const MIN_NUMBER=15;
 // const multiple=6;
 // let count=0;
+// const NUMBER_CONDITIONS=function(val){
+//     return val>=MIN_NUMBER && val<=MAX_NUMBER && val%multiple===0;
+// }
 
 // while(true)
 // {
 //     const inputNumber=prompt('enter number')
+//     if (NUMBER_CONDITIONS(inputNumber)) {
+//         console.log("You win!");
+//         break;
+//     }
 //     count++;
 //     if(count>2){
 //         console.log("error");
 //         break;
 //     }
-//     if (inputNumber<=MIN_NUMBER && inputNumber>=MAX_NUMBER && inputNumber%multiple===0) {
-//         console.log("You win!");
-//         break;
-//     }
 // }
 
-// const num1 =12;
-// const num2=45;
 
-// while(true){
-//     const userInput = prompt(`enter answer: ${num1}+${num2}=`);
-//     if(Number(userInput)=== num1+num2){
-//         console.log('Good');
-//         break;
-//     }
-// }
-
-const product = {
-    name: `dress`,
-    price: 123.44,
-    currency: `uah`,
-    showInfo: function () {
-        return`product name: ${this.name}
-        prece: ${this.currency} ${this.price}`
+const MAX_NUMBER=35;
+const MIN_NUMBER=15;
+const multiple=6;
+const NUMBER_CONDITIONS=function(val){
+    return val>=MIN_NUMBER && val<=MAX_NUMBER && val%multiple===0;
+}
+for (let i = 0; i < 3; i++) {
+    const inputNumber=prompt('enter number');
+    if (NUMBER_CONDITIONS(inputNumber)) {
+          console.log("You win!");
+         break;
+    }
+    if (i===2) {
+        console.log("error");
     }
 }
-console.log(product.showInfo());
