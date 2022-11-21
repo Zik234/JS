97dedee81a924ad10cf8cfc81d3cc1a61c5c0a75
Dima Tar <tarasenkodima338@gmail.com>
2022-11-21 11:52:27 +0200

    Class Work 21 11 23

--- MyArray.js
+++ MyArray.js
@@ -1,19 +1,27 @@
-// function MyArrayPrototype() {
-//   this.push = function(value){
-//     this[this.length] = value;
-//     this.length++;
-//     return this.length;
-// }
-// }
-
-// function MyArray() {
-//   this.length = 0;
-// }
-
-// MyArray.prototype = new MyArrayPrototype();
-
-// const array = [];//instance
-// console.log(array);
-
-// const myArray = new MyArray();
-// console.log(myArray);
+function MyArrayPrototype() {
+  this.push = function (value) {
+    this[this.length++] = value;
+    return this.length;
+  };
+  this.pop = function () {
+    //перевіряємо жовжину, щоб не стала від'ємною
+    if(this.length === 0){
+        return;
+    }
+    //отримати останній ш зберегти у змінну
+    const lastItem = this[this.length-1];
+    //видалити останній
+    delete this[this.length-1];
+    //змінити довжину
+    this.length--;
+    //повернути останній зі змінною
+    return lastItem;
+  }
+}
+function MyArray() {
+  this.length = 0
+}
+MyArray.prototype = new MyArrayPrototype()
+const myArr = new MyArray()
+myArr.push(12);
+myArr.push(56);
--- index.js
+++ index.js
@@ -1,21 +1,65 @@
-// Написати функцію, яка повертає об'єкт родина з властивостями, які мають вигляд:
-// ключ - ім'я члена родини, а значення - хто із членів родини
-// (мінімум родина з 3 осіб)
-// Інформацію про особу запитувати у користувача.
-// Вивести отриманий об'єкт за допомогою for..in
+// const arrNums1=[1,2,3];
+// const arrNums2=[9,8.7];
 
-const family = {
-  Tim: 'father',
-  Adam: 'son',
-  Eva: 'mother',
-  Reichel: 'daughter',
-  Vilyam: 'grandfather',
-}
+// //const resultArr = arrNums1.concat(arrNums2);
 
-for (const key in family) {
-  const inputNameFamily = prompt("enter a family member's name")
-  if (inputNameFamily === key) {
-    console.log(`${key}:${family[key]}`)
-    break
+// const newArray = new Array(5)
+// .fill(0)
+// .concat(14);
+
+// const deletedElement = arrNums1.pop();
+
+// const deletedElementFirs = arrNums2.shift();
+
+// arrNums1.unshift(15,45,78);
+
+// const languages = ['ua','fr','pl','en'];
+
+// const firstLang = languages.shift();
+// languages.push(firstLan
+
+const languages = ['ua', 'ua', 'en', 'pl', 'fr']
+
+//join
+const string = languages.join('^')
+// //includes
+// const checkLeng = languages.includes('pl');//повертає true false
+
+// if(checkLeng){
+//   console.log('dzen\' dobruy')
+// }else{
+//   console.log('error');
+// }
+
+// //reverse
+
+// const reverseLanguages = languages.reverse();//повертає address
+// languages.unshift('fr');
+
+// //indexOf
+// const findIndex1 = languages.indexOf('ua')//повертає перший індекс що є
+// console.log(findIndex1);
+// const findIndex2 = languages.lastIndexOf('ua')//повертає останній індекс що є
+// console.log(findIndex2);
+
+//задача
+// const checkKey = function(array, key){
+//   return array.includes(key);
+// };
+// console.log(checkKey(languages, 'fr'))
+
+//написати функцію яка приймає масив і повертае тру
+//якщо в масиві є два однакові значення інакше фолз
+
+function checkKeysInRow(array) {
+  //для кожного елементу треба зробити наступну перевірку
+  for (let index = 0; index < array.languages; index++) {
+    //поточний === наступний
+    if (array[index] === array[index + 1]) {
+      //якщо правда то завершити роботу і повернути true
+      return true
+    }
+    //якщо дійшли до кінця і не знайшли поспіль однакових false
+    return false
   }
 }
