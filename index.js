fe1ad23d3d42194aec8f46f65324108c483e8b28
Dima Tar <tarasenkodima338@gmail.com>
2022-11-28 17:36:23 +0200

    Home Work 28.11.23

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
-// написати скрипт, який створює об'єкт Товар з
-// властивостями:  назва, ціна, валюта
-// методами: показати усю інформацію про товар,
-// установити знижку на ціну і повернути нову ціну
-// з урахуванням знижки
+// написати скрипт (можна функцію можна без), який виводить індекс
+//  максимального елемента в масиві. Якщо максимальних елементів
+//   декілька(значення однакові) - виводить індекс останнього
+// const arrNums = [1, 9, 5, 6 , 7, 9, 4, 6]; => індекс 5
 
-// скрипт, який створює об'єкт Товар з
-// властивостями:  назва, ціна, валюта
-function Goods(name, price, currency) {
-  this.name = name
-  this.price = price
-  this.currency = currency
-}
+// * повертати кількість максимальних(однакових) елементів
+// const arrNums = [1, 9, 5, 6 , 7, 9, 4, 6]; =>кількість  2
 
-// методи
+const arrNums = [1, 9, 5, 6, 7, 9, 6] //cтворюю масив
 
-function MetodsGoods() {
-  this.ShowInfoProduct = function () {
-    // метод інформація про продукт
-    return `Information about product:
-name : ${this.name},
-prise: ${this.price} ${this.currency}`
-  }
-
-  this.DiscountPrice = function (value) {
-    // метод встановлення знижки
-    if (value > 0 && value <= 100) {
-      return `Discount on ${this.name}: ${value}%`
+function checkArrNums(array) {
+  let checkIndex = 0 //створюю максимальне значення
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] >= array[checkIndex]) {
+      checkIndex = index
     }
-    console.log('Discount cannot be accepted')
-    return NuN;
   }
+  return checkIndex
+}
+console.log(
+  'arrNums = [',
+  arrNums.join(', '),
+  '] => index',
+  checkArrNums(arrNums),
+)
 
-  this.DiscountProduct = function (value2) {
-    //метод повертаючий товар зі знижкою
-    this.price = this.price - (this.price * value2) / 100
-    return `Information about product on discount:
-name : ${this.name},
-prise: ${this.price} ${this.currency}`
+function checkMaxElement(array) {
+  let maxElement = 0
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] > array[maxElement]) {
+      maxElement = array[index]
+    }
   }
+  return maxElement
 }
 
-Goods.prototype = new MetodsGoods()
+function changeMaxElement(array){
+  let maxElement=checkMaxElement(array)
+  let count = 0
+  for (let index = 0; index < array.length; index++) {
+    if(maxElement === array[index]){
+      count++
+    }
+  }
+  return count
+}
 
-const dishes = new Goods('cup', 40, 'UAH') // створив продукт
-console.log(dishes.ShowInfoProduct()) // вивів інформацію про продукт
-const discount = prompt('enter discount') // користувач ввів знижку
-console.log(dishes.DiscountPrice(discount)) //виводимо перевірку можливої знижки
-console.log(dishes.DiscountProduct(discount))//виводимо інформацію зі знижкою
+console.log(
+  'arrNums = [',
+  arrNums.join(', '),
+  '] => number',
+  changeMaxElement(arrNums),
+)
