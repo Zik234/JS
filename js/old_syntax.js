d3a0b48acb5f3922d0e4fadbea35f230b1754ef4
Dima Tar <tarasenkodima338@gmail.com>
2022-12-03 15:51:12 +0200

    Add old syntax create constrruct obj

---
+++ js/old_syntax.js
@@ -0,0 +1,23 @@
+function User(fname, iname, age) {
+  this.fname = fname
+  this.iname = iname
+  this.age = age
+}
+//static
+User.isUser = function (value) {
+  return (value instanceof User)
+}
+
+function UserPrototype() {
+  this.fullName = function () {
+    return `${this.fname} ${this.iname}`
+  }
+}
+
+User.prototype = new UserPrototype()
+
+const obj = {}
+const persone1 = new User('Bred', 'Pitt', 58)
+console.log(persone1.fullName())
+console.log(User.isUser(persone1))
+console.log(User.isUser(obj))
