b290a3690c5fbb219b03988f20b2b639b207ba48
Dima Tar <tarasenkodima338@gmail.com>
2022-12-05 01:40:50 +0200

    Home Work Squirrel

--- js/Moderator.js
+++ js/Moderator.js
@@ -1,43 +1,3 @@
-// class Moderator extends UserClasses {
-//   constructor(fname, iname, age, reting) {
-//     super(fname, iname, age)
-//     this.reting = reting
-//   }
-//   applyMessage() {
-//     return 'apply'
-//   }
-// }
-
-// class Admin extends Moderator {
-//   constructor(fname, iname, age, reting, propAdmin) {
-//     super(fname, iname, age, reting)
-//     this.propAdmin = propAdmin
-//   }
-//   ban(user) {
-//     if (UserClasses.isUserClasses(user)) {
-//       return user.isBan = true
-//     }
-//     throw new TypeError('instance must be user')
-//   }
-//   unBan(user) {
-//     if (UserClasses.isUserClasses(user)) {
-//       return user.isBan = false
-//     }
-//     throw new TypeError('instance must be user')
-//   }
-// }
-
-// try {
-//   const moder = new Moderator('Moder', 'Moderator', 55, 4.5)
-//   console.log(moder.fullName())
-//   console.log(moder)
-//   const admin = new Admin('Ad', 'Admin', 22, 5.0, 'red')
-//   admin.ban(moder)
-//   console.log(moder.isBan)
-// } catch (error) {
-//   console.log(error)
-// }
-
 /*
 Squirrel
 -name(string) зробити setter&getter
@@ -64,81 +24,81 @@ MagicSquirrel
 створити екземпляр кожної білки, викликати на кожній білці усі доступні їй методи
 використати конструкцію try/catch
 */
-// class Squirrel {
-//   /**
-//    *
-//    * @param {string} name
-//    * @param {string} color
-//    * @param {number} jump
-//    */
-//   constructor(name, color) {
-//     this.name = name
-//     this.color = color
-//   }
-//   get name() {
-//     return this._name
-//   }
-//   set name(name) {
-//     if (typeof name !== 'string') {
-//       throw new TypeError('name must be string')
-//     }
-//     this._name = name
-//   }
-//   nameSquirrel() {
-//     return `${this._name}, ${this.color}`
-//   }
-//   jump() {
-//     return `${this._name} jump`
-//   }
-// }
+class Squirrel {
+  /**
+   *
+   * @param {string} name
+   * @param {string} color
+   * @param {number} jump
+   */
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+  get name() {
+    return this._name
+  }
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be string')
+    }
+    this._name = name
+  }
+  nameSquirrel() {
+    return `${this._name}, ${this.color}`
+  }
+  jump() {
+    return `${this._name} jump`
+  }
+}
 
-// class FlySquirrel extends Squirrel {
-//   /**
-//    * @param {number} maxFlyLength
-//    */
-//   constructor(name, color, maxFlyLength = 450) {
-//     super(name, color)
-//     this.maxFlyLength = maxFlyLength
-//   }
-//   fly(length) {
-//     if (length > this.maxFlyLength || length < 0) {
-//       throw new RangeError('Flyquirrel fly 0...150m')
-//     }
-//     if (typeof length !== 'number') {
-//       throw new TypeError('length mustbe be number')
-//     }
-//     return `${this.name} is fly at ${length}m.`
-//   }
-// }
+class FlySquirrel extends Squirrel {
+  /**
+   * @param {number} maxFlyLength
+   */
+  constructor(name, color, maxFlyLength = 450) {
+    super(name, color)
+    this.maxFlyLength = maxFlyLength
+  }
+  fly(length) {
+    if (length > this.maxFlyLength || length < 0) {
+      throw new RangeError('Flyquirrel fly 0...150m')
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('length mustbe be number')
+    }
+    return `${this.name} is fly at ${length}m.`
+  }
+}
 
-// class MagicSquirrel extends FlySquirrel {
-//   /**
-//    * @param {[string]} words
-//    */
-//   constructor(name, color, words) {
-//     super(name, color)
-//     this.words = words
-//   }
-//   sayWords() {
-//     for (let index = 0; index < this.words.length; index++) {
-//       console.log(this.words[index])
-//     }
-//   }
-// }
+class MagicSquirrel extends FlySquirrel {
+  /**
+   * @param {[string]} words
+   */
+  constructor(name, color, words) {
+    super(name, color)
+    this.words = words
+  }
+  sayWords() {
+    for (let index = 0; index < this.words.length; index++) {
+      console.log(this.words[index])
+    }
+  }
+}
 
-// try {
-//   const Squirrel2 = new Squirrel('Bob', 'red')
-//   console.log(Squirrel2.nameSquirrel())
-//   console.log(Squirrel2.jump())
-//   const FlySquirrel2 = new FlySquirrel('Tom', 'gren')
-//   console.log(FlySquirrel2.nameSquirrel())
-//   console.log(FlySquirrel2.jump())
-//   console.log(FlySquirrel2.fly(45))
-//   const MagicSquirrel2 = new MagicSquirrel('Vitya', 'bleak', ['a','b','c'])
-//   console.log(MagicSquirrel2.nameSquirrel())
-//   console.log(MagicSquirrel2.jump())
-//   console.log(MagicSquirrel2.fly(100))
-//   console.log(MagicSquirrel2.sayWords())
-// } catch (error) {
-//   console.log(error)
-// }
+try {
+  const Squirrel2 = new Squirrel('Bob', 'red')
+  console.log(Squirrel2.nameSquirrel())
+  console.log(Squirrel2.jump())
+  const FlySquirrel2 = new FlySquirrel('Tom', 'gren')
+  console.log(FlySquirrel2.nameSquirrel())
+  console.log(FlySquirrel2.jump())
+  console.log(FlySquirrel2.fly(45))
+  const MagicSquirrel2 = new MagicSquirrel('Vitya', 'bleak', ['a','b','c'])
+  console.log(MagicSquirrel2.nameSquirrel())
+  console.log(MagicSquirrel2.jump())
+  console.log(MagicSquirrel2.fly(100))
+  console.log(MagicSquirrel2.sayWords())
+} catch (error) {
+  console.log(error)
+}
--- js/index.js
+++ js/index.js
@@ -1,138 +1,138 @@
 'use strict'
 
-//інкапсуляція (абстракція)
-//наслідування
-//поліморфізм
+// //інкапсуляція (абстракція)
+// //наслідування
+// //поліморфізм
 
-// Створити клас Товар з властивостями: назва, ціна, валюта, кількість,
-// і методами: повернути інформацію про товар і купити товар(метод приймає кількість одиниць товару і повертає суму).
+// // Створити клас Товар з властивостями: назва, ціна, валюта, кількість,
+// // і методами: повернути інформацію про товар і купити товар(метод приймає кількість одиниць товару і повертає суму).
 
-// Створити дочірні класи Фізичний товар і Віртуальний товар, обидва походять від класу товар.
-// У фізичного товара додається властивість вага.
-// У віртуального товара додається властивість розмір пам'яті.
+// // Створити дочірні класи Фізичний товар і Віртуальний товар, обидва походять від класу товар.
+// // У фізичного товара додається властивість вага.
+// // У віртуального товара додається властивість розмір пам'яті.
 
-class Goods {
-  //constructor object
-  /**
-   *
-   * @param {string} name
-   * @param {number} price
-   * @param {string} currency
-   * @param {number} number
-   */
-  constructor(name, price, currency, number) {
-    if (typeof name !== 'string') {
-      throw new TypeError('fname mustbe string')
-    }
-    if (typeof price !== 'number') {
-      throw new TypeError('iname mustbe string')
-    }
-    if (typeof currency !== 'string') {
-      throw new TypeError('iname mustbe string')
-    }
-    if (typeof number !== 'number') {
-      throw new TypeError('iname mustbe string')
-    }
-    this.name = name
-    this.price = price
-    this.currency = currency
-    this.number = number //seter
-  }
-  get price() {
-    return this._price
-  }
-  set price(price) {
-    if (price <= 0) {
-      throw new RangeError('price must >0')
-    }
-    this._price = price
-  }
-  get number() {
-    return this._number
-  }
-  set number(number) {
-    if (number <= 0) {
-      throw new RangeError('number must >0')
-    }
-    this._number = number
-  }
-  InformGoods() {
-    return `${this.name}:${this._price}(${this.currency}) | ${this._number} pieces`
-  }
-  BuyGoods(num) {
-    if (num <= 0) {
-      throw new RangeError('number must >0')
-    }
-    if (num === undefined) {
-      return `${this._price * this._number}(${this.currency})`
-    } else if (typeof num !== 'number') {
-      throw new TypeError('iname mustbe string')
-    }
-    return `${this.name}:${this._price}(${this.currency}) | ${num} pieces\n${
-      this._price * num
-    }(${this.currency})`
-  }
-}
-class Physicalproduct extends Goods {
-  constructor(name, price, currency, number, weight) {
-    super(name, price, currency, number)
-    if (typeof weight !== 'number') {
-      throw new TypeError('fname mustbe number')
-    }
-    this.weight = weight
-  }
-  get weight() {
-    return this._weight
-  }
-  set weight(weight) {
-    if (weight <= 0) {
-      throw new RangeError('number must >0')
-    }
-    this._weight = weight
-  }
-  InformGoodsPhysical() {
-    return `${this.name}:${this.price}(${this.currency}) | ${this.number} pieces | ${this._weight} kg`}
-  weightGoods() {
-    return `${this.name} - ${this.number * this._weight} kg`
-  }
-}
+// class Goods {
+//   //constructor object
+//   /**
+//    *
+//    * @param {string} name
+//    * @param {number} price
+//    * @param {string} currency
+//    * @param {number} number
+//    */
+//   constructor(name, price, currency, number) {
+//     if (typeof name !== 'string') {
+//       throw new TypeError('fname mustbe string')
+//     }
+//     if (typeof price !== 'number') {
+//       throw new TypeError('iname mustbe string')
+//     }
+//     if (typeof currency !== 'string') {
+//       throw new TypeError('iname mustbe string')
+//     }
+//     if (typeof number !== 'number') {
+//       throw new TypeError('iname mustbe string')
+//     }
+//     this.name = name
+//     this.price = price
+//     this.currency = currency
+//     this.number = number //seter
+//   }
+//   get price() {
+//     return this._price
+//   }
+//   set price(price) {
+//     if (price <= 0) {
+//       throw new RangeError('price must >0')
+//     }
+//     this._price = price
+//   }
+//   get number() {
+//     return this._number
+//   }
+//   set number(number) {
+//     if (number <= 0) {
+//       throw new RangeError('number must >0')
+//     }
+//     this._number = number
+//   }
+//   InformGoods() {
+//     return `${this.name}:${this._price}(${this.currency}) | ${this._number} pieces`
+//   }
+//   BuyGoods(num) {
+//     if (num <= 0) {
+//       throw new RangeError('number must >0')
+//     }
+//     if (num === undefined) {
+//       return `${this._price * this._number}(${this.currency})`
+//     } else if (typeof num !== 'number') {
+//       throw new TypeError('iname mustbe string')
+//     }
+//     return `${this.name}:${this._price}(${this.currency}) | ${num} pieces\n${
+//       this._price * num
+//     }(${this.currency})`
+//   }
+// }
+// class Physicalproduct extends Goods {
+//   constructor(name, price, currency, number, weight) {
+//     super(name, price, currency, number)
+//     if (typeof weight !== 'number') {
+//       throw new TypeError('fname mustbe number')
+//     }
+//     this.weight = weight
+//   }
+//   get weight() {
+//     return this._weight
+//   }
+//   set weight(weight) {
+//     if (weight <= 0) {
+//       throw new RangeError('number must >0')
+//     }
+//     this._weight = weight
+//   }
+//   InformGoodsPhysical() {
+//     return `${this.name}:${this.price}(${this.currency}) | ${this.number} pieces | ${this._weight} kg`}
+//   weightGoods() {
+//     return `${this.name} - ${this.number * this._weight} kg`
+//   }
+// }
 
-class Virtualproduct extends Goods {
-  constructor(name, price, currency, number, memorySize) {
-    super(name, price, currency, number)
-    if (typeof memorySize !== 'number') {
-      throw new TypeError('fname mustbe number')
-    }
-    this.memorySize = memorySize
-  }
-  get memorySize() {
-    return this._memorySize
-  }
-  set memorySize(memorySize) {
-    if (memorySize <= 0) {
-      throw new RangeError('number must >0')
-    }
-    this._memorySize = memorySize
-  }
-  InformGoodsVirtual() {
-    return `${this.name}:${this.price}(${this.currency}) | ${this.number} pieces | ${this._memorySize} mBt`}
-    memorySizeGoods() {
-    return `${this._memorySize} mBt`
-  }
-}
+// class Virtualproduct extends Goods {
+//   constructor(name, price, currency, number, memorySize) {
+//     super(name, price, currency, number)
+//     if (typeof memorySize !== 'number') {
+//       throw new TypeError('fname mustbe number')
+//     }
+//     this.memorySize = memorySize
+//   }
+//   get memorySize() {
+//     return this._memorySize
+//   }
+//   set memorySize(memorySize) {
+//     if (memorySize <= 0) {
+//       throw new RangeError('number must >0')
+//     }
+//     this._memorySize = memorySize
+//   }
+//   InformGoodsVirtual() {
+//     return `${this.name}:${this.price}(${this.currency}) | ${this.number} pieces | ${this._memorySize} mBt`}
+//     memorySizeGoods() {
+//     return `${this._memorySize} mBt`
+//   }
+// }
 
-try {
-  const Goods1 = new Goods('Hous', 23, 'UAH', 5)
-  console.log(Goods1)
-  console.log(Goods1.InformGoods())
-  console.log(Goods1.BuyGoods())
-  console.log(Goods1.BuyGoods(2))
-  const Goods2 = new Physicalproduct('biff',55,'EU',10, 2)
-  console.log(Goods2.InformGoodsPhysical())
-  console.log(Goods2.weightGoods())
-  const Goods3 = new Virtualproduct('chick',12,'FR',43, 7)
-  console.log(Goods3.InformGoodsVirtual())
-  console.log(Goods3.memorySizeGoods())
-} catch (error) {
-  console.log(error)
-}
+// try {
+//   const Goods1 = new Goods('Hous', 23, 'UAH', 5)
+//   console.log(Goods1)
+//   console.log(Goods1.InformGoods())
+//   console.log(Goods1.BuyGoods())
+//   console.log(Goods1.BuyGoods(2))
+//   const Goods2 = new Physicalproduct('biff',55,'EU',10, 2)
+//   console.log(Goods2.InformGoodsPhysical())
+//   console.log(Goods2.weightGoods())
+//   const Goods3 = new Virtualproduct('chick',12,'FR',43, 7)
+//   console.log(Goods3.InformGoodsVirtual())
+//   console.log(Goods3.memorySizeGoods())
+// } catch (error) {
+//   console.log(error)
+// }
